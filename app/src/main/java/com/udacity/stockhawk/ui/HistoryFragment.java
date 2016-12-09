d443037ec5f1efa7ea396486a106af9af08a807e
Riberto Junior <ribertojunior@gmail.com>
2016-12-09 15:17:24 -0200

    Chart styling

--- app/src/main/java/com/udacity/stockhawk/ui/HistoryFragment.java
+++ app/src/main/java/com/udacity/stockhawk/ui/HistoryFragment.java
@@ -8,18 +8,23 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.StringTokenizer;
 
 
@@ -49,7 +54,23 @@ public class HistoryFragment extends Fragment implements LoaderManager.LoaderCal
         View rootView = inflater.inflate(R.layout.fragment_history, container, false);
         mChart = (LineChart) rootView.findViewById(R.id.chart);
         mChart.setDescription(getString(R.string.chart_description));
+        int color = getResources().getColor(R.color.white);
+        mChart.setDescriptionColor(color);
+        mChart.setDescriptionTextSize(16);
+        //
+        mChart.getLegend().setTextColor(color);
 
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setAxisLineColor(color);
+        xAxis.setTextColor(color);
+
+        YAxis yAxis = mChart.getAxisRight();
+        yAxis.setAxisLineColor(color);
+        yAxis.setTextColor(color);
+
+        yAxis = mChart.getAxisLeft();
+        yAxis.setAxisLineColor(color);
+        yAxis.setTextColor(color);
         return rootView;
     }
 
@@ -75,21 +96,34 @@ public class HistoryFragment extends Fragment implements LoaderManager.LoaderCal
             ArrayList<String> xValues = new ArrayList<>();
             String history = data.getString(COL_HISTORY_ID);
             String value;
+            long date;
             StringTokenizer tokenizer = new StringTokenizer(history, "\n");
             int x = 0;
-            int count = tokenizer.countTokens() - 1;
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            // we start at the day returned by local time. Otherwise this is a mess.
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            // now we work exclusively in UTC
+            dayTime = new Time();
+            long day;
+            SimpleDateFormat monthDayFormat = new SimpleDateFormat(getString(R.string.date_format), Locale.US);
             while (tokenizer.hasMoreTokens()){
-                xValues.add(""+(count-x));
                 value = tokenizer.nextToken();
+                day = dayTime.setJulianDay(julianStartDay + x);
+                xValues.add(monthDayFormat.format(day));
                 value = value.split(",")[1];
                 entries.add(new Entry(Float.parseFloat(value), x));
                 x++;
             }
             LineDataSet dataSet = new LineDataSet(entries, Contract.Quote.getStockFromUri(mSymbolUri));
             dataSet.setColors(new int[]{R.color.white}, getContext());
-            dataSet.setValueTextColor(R.color.white);
+            dataSet.setValueTextColor(getResources().getColor(R.color.white));
+
 
             LineData lineData = new LineData(xValues, dataSet);
+
 
             mChart.setData(lineData);
 
